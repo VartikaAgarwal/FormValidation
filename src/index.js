1acc93ff8b8afbe5b4d88390d605cfe298dbf9fe
VartikaAgarwal <124383384+VartikaAgarwal@users.noreply.github.com>
2025-06-08 15:00:13 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import FormComponent from './components/FormComponent';
 import "./styles.css"
+import 'font-awesome/css/font-awesome.min.css';
 
 const rootElement = document.getElementById("root");
 
